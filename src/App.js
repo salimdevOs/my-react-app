8e9e5d62ccc0c7df4c57efd6484181fc39bd71f8
Rai Saleem Ur Rehman <66565091+salimdevOs@users.noreply.github.com>
2020-08-25 23:02:56 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,10 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        
         >
         <h1>hello saleem</h1>
         </a>
